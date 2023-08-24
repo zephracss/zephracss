e30f63aeaa48faa002145e7655d471a5ef342857
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-08-24 20:56:19 +1000

    (preset-default) Update positioning

--- packages/preset-default/src/rules/positioning.ts
+++ packages/preset-default/src/rules/positioning.ts
@@ -24,3 +24,72 @@ export const inset: Rule = {
         };
     },
 };
+
+export const insetX: Rule = {
+    match: new RegExp(`inset-x-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        const value = Units.from(config, size);
+
+        return {
+            right: value,
+            left: value,
+        };
+    },
+};
+
+export const insetY: Rule = {
+    match: new RegExp(`inset-y-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        const value = Units.from(config, size);
+
+        return {
+            top: value,
+            bottom: value,
+        };
+    },
+};
+
+export const top: Rule = {
+    match: new RegExp(`top-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        return {
+            top: Units.from(config, size),
+        };
+    },
+};
+
+export const right: Rule = {
+    match: new RegExp(`right-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        return {
+            right: Units.from(config, size),
+        };
+    },
+};
+
+export const bottom: Rule = {
+    match: new RegExp(`bottom-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        return {
+            bottom: Units.from(config, size),
+        };
+    },
+};
+
+export const left: Rule = {
+    match: new RegExp(`left-${trailing(Units.match.source)}`),
+    generate: (config, _, size) => {
+        return {
+            left: Units.from(config, size),
+        };
+    },
+};
+
+export const zIndex: Rule = {
+    match: new RegExp(`z-(\\d+)`),
+    generate: (_, __, size) => {
+        return {
+            zIndex: size,
+        };
+    },
+};
